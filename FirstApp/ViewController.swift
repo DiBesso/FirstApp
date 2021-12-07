e44e6074e7ab47534641aeee80f54c3a12a32c35
Dmitry <radiator211@mail.ru>
2021-12-07 20:23:27 +0700

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Дмитрий Бессонов on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
